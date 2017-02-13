27fd4773206120bbe62d2e4440363ab6a73c0996
bravokeyl <bravokeyl@gmail.com>
2017-02-13 18:28:53 +0530

    shortcode

--- bk/helpers.php
+++ bk/helpers.php
@@ -2,13 +2,35 @@
 function bk_add_to_cart( $atts ) {
   $atts = shortcode_atts( array(
         'label' => 'Add to Cart',
-        'id' => ''
+        'id' => '',
+        'type' => 'upgrade'
   ), $atts, 'fable_cart' );
+
   $pid = intval($atts['id']);
-  $url = '<a rel="nofollow" href="/cart/?add-to-cart='.$pid.'" data-quantity="1"';
-  $url .= 'data-product_id="'.$pid.'" class="grve-btn grve-btn-medium grve-round grve-bg-primary-1 grve-bg-hover-black
-product_type_simple add_to_cart_button">><span class="grve-item">';
-  $url .= '<i class="grve-menu-icon fa fa-shopping-cart"></i></span>'.$atts['label'].'</a>';
+  $class = "grve-btn grve-btn-medium grve-round grve-bg-primary-1";
+  $href= '';
+  $eligible= true;
+
+  if( "new" === $atts['type'] ){
+    $href = 'href="/cart/?add-to-cart='.$pid.'"';
+  } else {
+    $href = 'href="/my-account/"';
+    $atts['label'] = "Login to upgrade";
+  }
+
+  if( ! is_user_logged_in() ){
+    $atts['label'] = "Login to upgrade";
+    $url = '<a '.$href.' class="'.$class.'">'.$atts['label'].'</button>';
+  } else{
+    $href = 'href="/cart/?add-to-cart='.$pid.'"';
+    if($eligible){
+      $url = '<a data-quantity="1" '.$href;
+      $url .= 'data-product_id="'.$pid.'" class="'.$class.'">><span class="grve-item">';
+      $url .= '<i class="grve-menu-icon fa fa-shopping-cart"></i></span>'.$atts['label'].'</a>';
+    } else {
+      $url = '<button type="button" class="disabled grve-bg-hover-none" disabled>Not eligible to upgrade</button>';
+    }
+  }
 
   return $url;
 }
