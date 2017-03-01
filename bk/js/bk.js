79c6ef80241b618be70c5cd215d955d4f6536e04
bravokeyl <bravokeyl@gmail.com>
2017-03-01 12:21:28 +0530

    Direct paste

--- bk/js/bk.js
+++ bk/js/bk.js
@@ -1,11 +1,52 @@
 (function($){
-  $(".bk-form .serial-input").keyup(function () {
-    if (this.value.length == this.maxLength) {
-      var $next = $(this).next('.serial-input');
-      if ($next.length)
-          $(this).next('.serial-input').focus();
-      else
-          $(this).blur();
+  var sip = $(".bk-form .serial-input");
+  var sipf = $(".bk-form .serial-input#bk_serial_key1");
+  // sip.not("#bk_serial_key1").keyup(function () {
+  //   console.log("Key UP");
+  //   if (this.value.length == this.maxLength) {
+  //     var $next = $(this).next('.serial-input');
+  //     if ($next.length)
+  //         $(this).next('.serial-input').focus();
+  //     else
+  //         $(this).blur();
+  //   }
+  // });
+  sipf.keyup(function () {
+    console.log("First add");
+    if (this.value.length == this.maxLength || (24 == this.value.length) || (21 == this.value.length) || (20 == this.value.length)) {
+      console.log("Max Characters",this.value);
+      var ips = [];
+      if((25 == this.value.length)){
+        ips = this.value.split('-');
+      }
+      if((24 == this.value.length)){
+        ips = this.value.split('-');
+      }
+      if((21 == this.value.length)){
+        ips[0] = this.value.substring(0,5);
+        var sps = this.value.substring(5).match(/.{1,4}/g);
+        ips = ips.concat(sps);
+      }
+      if((20 == this.value.length)){
+        ips = this.value.match(/.{1,4}/g); //split at every 4 characters
+      }
+      // console.log(ips,sps);
+      if( 5 == ips.length){
+        $("#bk_serial_key1").val(ips[0]);
+        $("#bk_serial_key2").val(ips[1]);
+        $("#bk_serial_key3").val(ips[2]);
+        $("#bk_serial_key4").val(ips[3]);
+        $("#bk_serial_key5").val(ips[4]);
+      }
+      // var $next = $(this).next('.serial-input');
+      // if ($next.length)
+      //     $(this).next('.serial-input').focus();
+      // else
+      //     $(this).blur();
     }
   });
+  $("#clear-form").on("click",function(e){
+    e.preventDefault();
+    $(".bk-form .serial-input").val("");
+  })
 })(jQuery);
--- woocommerce/myaccount/register-keys.php
+++ woocommerce/myaccount/register-keys.php
@@ -5,7 +5,7 @@
 <form action="" method="post" class="bk-form">
   <p class="woocommerce-FormRow woocommerce-FormRow--first form-row">
     <label for="bk_serial_key"><?php _e( 'Register serial number', 'bk' ); ?> <span class="required">*</span></label>
-    <input type="text" required maxLength="5" class="serial-input woocommerce-Input woocommerce-Input--text input-text bk-small" name="bk_serial_key1" id="bk_serial_key1"
+    <input type="text" required maxLength="25" class="serial-input woocommerce-Input woocommerce-Input--text input-text bk-small" name="bk_serial_key1" id="bk_serial_key1"
     value="" />  &nbsp;&ndash;&nbsp;
     <input type="text" maxLength="4" required class="serial-input woocommerce-Input woocommerce-Input--text input-text bk-small"
     pattern="[A-Za-z0-9]{4}"title="Four digit alphanumeric" name="bk_serial_key2" id="bk_serial_key2" value="" />  &nbsp;&ndash;&nbsp;
@@ -15,6 +15,7 @@
     pattern="[A-Za-z0-9]{4}"title="Four digit alphanumeric" name="bk_serial_key4" id="bk_serial_key4" value="" />  &nbsp;&ndash;&nbsp;
     <input type="text" maxLength="4" required class="serial-input woocommerce-Input woocommerce-Input--text input-text bk-small"
     pattern="[A-Za-z0-9]{4}"title="Four digit alphanumeric" name="bk_serial_key5" id="bk_serial_key5" value="" />
+    <a href="#" id="clear-form">Clear</a>
   </p>
   <div class="clear"></div>
   <p>
